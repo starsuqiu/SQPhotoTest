7da9039c584179cdc82fdbd7a59521a39041c8a6
starsuqiu <260824093@qq.com>
2018-01-12 14:30:08 +0800

    commit

---
+++ SQPhotoTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SQPhotoTest
+//
+//  Created by Qiu on 2018/1/12.
+//  Copyright © 2018年 Qiu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SQPhotoTest/ViewController.h
@@ -0,0 +1,16 @@
+//
+//  ViewController.h
+//  SQPhotoTest
+//
+//  Created by Qiu on 2018/1/12.
+//  Copyright © 2018年 Qiu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+
+@end
+
